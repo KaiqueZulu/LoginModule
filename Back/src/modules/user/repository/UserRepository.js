e100019c6cd3434d9d4412b0046862fc06433ca1
KaiquePires <manozulukid@gmail.com>
2022-12-20 05:57:30 +0000

    creating signature of CRUD functions

--- Back/src/modules/user/repository/UserRepository.js
+++ Back/src/modules/user/repository/UserRepository.js
@@ -1,12 +1,32 @@
+import bcrypt from "bcrypt";
 import User from "../model/User.js";
 
 class UserRepository {
+    async createUser(name, email, password) {
+        let hashPassword = await bcrypt.hash(password, 10);
+        return User.create({
+            name: name,
+            email: email,
+            password: hashPassword
+        });
+    };
+
     findByEmail(email) {
         return User.findOne({
             where: { email }
         });
     };
-    
+
+    updateUser(userToUpdate){
+        return User.upsert(userToUpdate);
+    };
+
+    dropUser(email){
+        return User.destroy({
+            where: {email}
+        });
+    };
+
 };
 
 export default new UserRepository();
