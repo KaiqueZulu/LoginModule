c8b71d6d56e4a37d725e34c26f200067c8908d99
Kaique Pires <manozulukid@gmail.com>
2023-02-05 21:24:00 -0300

    swagger consig

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,11 +1,16 @@
 import express from "express"
+import swaggerUi from 'swagger-ui-express';
+import { readFile } from 'fs/promises';
 import UserRoutes from "./modules/user/routes/UserRoutes.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+const swaggerDocs = JSON.parse(
+  await readFile('/home/matdev/LoginModule/Back/src/config/swagger/swagger.json', 'utf8')
+);
 
-app.use(express.json()) // for parsing application/json
+app.use(express.json())// for parsing application/json
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
@@ -15,6 +20,7 @@ app.get('/api/status', (req, res) => {
     });
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/user', UserRoutes);
 
 app.listen(PORT, () => {
